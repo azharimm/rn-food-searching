03a268e38a0498d75e4cab00be8f955a9109e5a0
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-11-30 08:16:02 +0700

    ^ useState hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,30 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
     const [results, setResults] = useState([])
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'san jose'
-            }
-        })
-        setResults(response.data.businesses)
+    const searchApi = async (term) => {
+        console.log('hi there')
+        try{
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose'
+                }
+            })
+            setResults(response.data.businesses)
+        }catch(e) {
+            console.log(e)
+            setErrorMessage('Something went wrong')
+        }
     }
-
+    useEffect(() => {
+        searchApi('pasta')
+    }, [])
     return (
         <View>
             <SearchBar 
             term={term} 
             onTermChange={setTerm}
-            onTermSubmit={searchApi} />
-            <Text>This will be the food list screen</Text>
+            onTermSubmit={(term) =>searchApi(term)} />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     )
