87a1bf2074c08f9414c84c161d068afcfb462159
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-11-30 10:18:41 +0700

    ^ styling

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from './ResultsDetail'
 
 const ResultList = ({title, results}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList 
             horizontal
@@ -18,9 +18,15 @@ const ResultList = ({title, results}) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10
+    },  
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 10
+        
     }
 })
 
